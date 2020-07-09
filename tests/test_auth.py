77d516ebd85e1eee89d4a09e4e0d28cf7464c9b8
animepacan <rly@datacorp.ee>
2020-07-09 17:35:07 +0300

    added auth permanent_login method

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,13 +5,22 @@ from neuroio import AsyncClient, Client
 from neuroio.constants import API_BASE_URL
 
 
+@pytest.fixture
+def client():
+    return Client()
+
+
+@pytest.fixture
+async def async_client():
+    return AsyncClient(api_token="anotherone")
+
+
 @respx.mock
-def test_login_200():
-    neuroio = Client()
+def test_login_200(client):
     request = respx.post(
         f"{API_BASE_URL}/v1/login/", status_code=200, content={"token": "key"}
     )
-    login_response = neuroio.auth.login(
+    login_response = client.auth.login(
         username="helloworld", password="superpwd"
     )
     assert request.called
@@ -21,12 +30,40 @@ def test_login_200():
 
 @respx.mock
 @pytest.mark.asyncio
-async def test_async_login_200():
-    neuroio = AsyncClient(api_token="anotherone")
+async def test_async_login_200(async_client):
     request = respx.post(
         f"{API_BASE_URL}/v1/login/", status_code=200, content={"token": "key"}
     )
-    login_response = await neuroio.auth.login(
+    login_response = await async_client.auth.login(
+        username="helloworld", password="superpwd"
+    )
+    assert request.called
+    assert login_response.status_code == 200
+    assert login_response.json()["token"] == "key"
+
+
+@respx.mock
+def test_login_permanent_200(client):
+    request = respx.post(
+        f"{API_BASE_URL}/v1/login/permanent/",
+        status_code=200, content={"token": "key"}
+    )
+    login_response = client.auth.login_permanent(
+        username="helloworld", password="superpwd"
+    )
+    assert request.called
+    assert login_response.status_code == 200
+    assert login_response.json()["token"] == "key"
+
+
+@respx.mock
+@pytest.mark.asyncio
+async def test_async_login_permanent_200(async_client):
+    request = respx.post(
+        f"{API_BASE_URL}/v1/login/permanent/",
+        status_code=200, content={"token": "key"}
+    )
+    login_response = await async_client.auth.login_permanent(
         username="helloworld", password="superpwd"
     )
     assert request.called
