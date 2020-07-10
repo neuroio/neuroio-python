06b5ae6bb474b91d653d7abce7efda60197e0b8e
Roma <BumagniyPacket@users.noreply.github.com>
2020-07-10 17:03:19 +0300

    added /users methods (#3)
    * added neuroio client no auth
    
    * tokens list request added
    
    * token info request added
    
    * token update request added
    
    * tokens delete request added
    
    * token delete request added
    
    * "password change" request added
    
    * "me" request added
    
    * tokens_delete: added permanent arg

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,20 @@ from neuroio import AsyncClient, Client
 
 
 @pytest.fixture
-def client():
+def client_no_auth():
     return Client()
 
 
 @pytest.fixture
-async def async_client():
+async def async_client_no_auth():
     return AsyncClient()
+
+
+@pytest.fixture
+def client():
+    return Client(api_token="token")
+
+
+@pytest.fixture
+async def async_client():
+    return AsyncClient(api_token="token")
