ab4288742020904f1e802ba790831ba4826d5943
Roma <BumagniyPacket@users.noreply.github.com>
2020-07-10 11:33:43 +0300

    added auth permanent_login method (#2)
    * added auth permanent_login method
    
    * linted
    
    * conftest.py added
    
    Co-authored-by: animepacan <rly@datacorp.ee>

--- neuroio/api/auth/v1.py
+++ neuroio/api/auth/v1.py
@@ -11,11 +11,27 @@ class Auth(APIBase):
         finally:
             self.client.close()
 
+    def login_permanent(self, username: str, password: str) -> Response:
+        data = {"username": username, "password": password}
+        try:
+            return self.client.post(url="/v1/login/permanent/", data=data)
+        finally:
+            self.client.close()
+
 
 class AuthAsync(APIBaseAsync):
     async def login(self, username: str, password: str) -> Response:
         data = {"username": username, "password": password}
         try:
             return await self.client.post(url="/v1/login/", data=data)
+        finally:
+            await self.client.aclose()
+
+    async def login_permanent(self, username: str, password: str) -> Response:
+        data = {"username": username, "password": password}
+        try:
+            return await self.client.post(
+                url="/v1/login/permanent/", data=data
+            )
         finally:
             await self.client.aclose()
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,17 +1,15 @@
 import pytest
 import respx
 
-from neuroio import AsyncClient, Client
 from neuroio.constants import API_BASE_URL
 
 
 @respx.mock
-def test_login_200():
-    neuroio = Client()
+def test_login_200(client):
     request = respx.post(
         f"{API_BASE_URL}/v1/login/", status_code=200, content={"token": "key"}
     )
-    login_response = neuroio.auth.login(
+    login_response = client.auth.login(
         username="helloworld", password="superpwd"
     )
     assert request.called
@@ -21,12 +19,42 @@ def test_login_200():
 
 @respx.mock
 @pytest.mark.asyncio
-async def test_async_login_200():
-    neuroio = AsyncClient(api_token="anotherone")
+async def test_async_login_200(async_client):
     request = respx.post(
         f"{API_BASE_URL}/v1/login/", status_code=200, content={"token": "key"}
     )
-    login_response = await neuroio.auth.login(
+    login_response = await async_client.auth.login(
+        username="helloworld", password="superpwd"
+    )
+    assert request.called
+    assert login_response.status_code == 200
+    assert login_response.json()["token"] == "key"
+
+
+@respx.mock
+def test_login_permanent_200(client):
+    request = respx.post(
+        f"{API_BASE_URL}/v1/login/permanent/",
+        status_code=200,
+        content={"token": "key"},
+    )
+    login_response = client.auth.login_permanent(
+        username="helloworld", password="superpwd"
+    )
+    assert request.called
+    assert login_response.status_code == 200
+    assert login_response.json()["token"] == "key"
+
+
+@respx.mock
+@pytest.mark.asyncio
+async def test_async_login_permanent_200(async_client):
+    request = respx.post(
+        f"{API_BASE_URL}/v1/login/permanent/",
+        status_code=200,
+        content={"token": "key"},
+    )
+    login_response = await async_client.auth.login_permanent(
         username="helloworld", password="superpwd"
     )
     assert request.called
