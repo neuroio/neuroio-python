09732c186b5df6450fc0c99f0ec196ea4b0b2dfd
BumagniyPacket <bumagniypacket@yandex.ru>
2020-08-04 14:35:06 +0300

    del login_permanent

--- neuroio/iam/auth/v1.py
+++ neuroio/iam/auth/v1.py
@@ -11,27 +11,11 @@ class Auth(APIBase):
         finally:
             self.client.close()
 
-    def login_permanent(self, username: str, password: str) -> Response:
-        data = {"username": username, "password": password}
-        try:
-            return self.client.post(url="/v1/login/permanent/", json=data)
-        finally:
-            self.client.close()
-
 
 class AuthAsync(APIBaseAsync):
     async def login(self, username: str, password: str) -> Response:
         data = {"username": username, "password": password}
         try:
             return await self.client.post(url="/v1/login/", json=data)
-        finally:
-            await self.client.aclose()
-
-    async def login_permanent(self, username: str, password: str) -> Response:
-        data = {"username": username, "password": password}
-        try:
-            return await self.client.post(
-                url="/v1/login/permanent/", json=data
-            )
         finally:
             await self.client.aclose()
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -29,34 +29,3 @@ async def test_async_login_200(async_client_no_auth):
     assert request.called
     assert login_response.status_code == 200
     assert login_response.json()["token"] == "key"
-
-
-@respx.mock
-def test_login_permanent_200(client_no_auth):
-    request = respx.post(
-        f"{IAM_BASE_URL}/v1/login/permanent/",
-        status_code=200,
-        content={"token": "key"},
-    )
-    login_response = client_no_auth.auth.login_permanent(
-        username="helloworld", password="superpwd"
-    )
-    assert request.called
-    assert login_response.status_code == 200
-    assert login_response.json()["token"] == "key"
-
-
-@respx.mock
-@pytest.mark.asyncio
-async def test_async_login_permanent_200(async_client_no_auth):
-    request = respx.post(
-        f"{IAM_BASE_URL}/v1/login/permanent/",
-        status_code=200,
-        content={"token": "key"},
-    )
-    login_response = await async_client_no_auth.auth.login_permanent(
-        username="helloworld", password="superpwd"
-    )
-    assert request.called
-    assert login_response.status_code == 200
-    assert login_response.json()["token"] == "key"
