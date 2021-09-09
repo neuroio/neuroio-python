72f2a8b1d0a1d7551567413c5134da89041e6181
Ekaterina Zaitseva <e.zaitseva@trinitygroup.ru>
2021-09-09 15:48:18 +0300

    inc test coverage

--- tests/test_sync_client.py
+++ tests/test_sync_client.py
@@ -9,3 +9,13 @@ def test_client_default_api_version():
 def test_client_api_version():
     neuroio = Client(api_version=2)
     assert neuroio.api_version == 2
+
+
+def test_get_incorrect_attr():
+    neuroio = Client(api_version=1)
+    in_exception = False
+    try:
+        neuroio.incorrect
+    except BaseException:
+        in_exception = True
+    assert in_exception
