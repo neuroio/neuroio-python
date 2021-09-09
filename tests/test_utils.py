26f5b6b747829bf85743f0de292e01621041369e
Alex Meyer <meier23@yandex.ru>
2021-09-09 14:59:03 +0300

    added tests for method prepare_image_process

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,53 @@
+import os
+
+import _io
+
 from neuroio.constants import sentinel
 from neuroio.utils import (
+    prepare_image_processing,
     process_query_params,
     request_dict_processing,
     request_form_processing,
     request_query_processing,
 )
+
+
+def test_prepare_image_processing_binary():
+    data = b"testdata"
+    name, image_data = prepare_image_processing(data)
+
+    assert isinstance(image_data, _io.BytesIO)
+    assert image_data.read() == data
+
+
+def test_prepare_image_processing_buffer_reader():
+    filename = "image.jpg"
+    data = b"test_image_data"
+
+    with open(filename, "wb") as f:
+        f.write(data)
+
+    with open(filename, "rb") as f:
+        result = prepare_image_processing(f).read()
+
+    os.remove(filename)
+
+    assert result == data
+
+
+def test_prepare_image_processing_tuple():
+    filename = "image.jpg"
+    data = b"test_image_data"
+
+    with open(filename, "wb") as f:
+        f.write(data)
+
+    with open(filename, "rb") as f:
+        result = prepare_image_processing((filename, f))[1].read()
+
+    os.remove(filename)
+
+    assert result == data
 
 
 def test_process_query_params():
